9a31f2c14d941a734f55c4bfcda66c022ef75369
Almantas Vorobjovas <almantasvorobjovas@Almantass-MacBook-Pro.local>
2024-04-17 20:30:01 +0300

    Fix register validation, Response codes, CORS

--- backend/src/main/java/lt/codecrusaders/backend/controllers/AuthController.java
+++ backend/src/main/java/lt/codecrusaders/backend/controllers/AuthController.java
@@ -8,10 +8,12 @@ import lt.codecrusaders.backend.repositories.UserRepository;
 import lt.codecrusaders.backend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 public class AuthController {
     @Autowired
